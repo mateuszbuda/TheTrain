6d359c9efa23fabf5b37bbc0cfe52cecd54f684c
Mateusz Buda <mateusz@antipattern.pl>
2017-01-19 00:18:31 -0500

    Railway generation function parametrized with L to R swiches ratio

--- generate_graph.m
+++ generate_graph.m
@@ -1,8 +1,10 @@
-function [ G ] = generate_graph( size, type )
-%GENERATE_GRAPH Generates railway graph of given size and type
+function [ railway ] = generate_graph( size, ratio )
+%GENERATE_GRAPH Generates railway graph of given size and L to R ratio
 %   size - the number of switches will be 2*size as there must be even
 %   number of them
-%   type - string; one of 'random', 'ergodic', 'loop'
+%   ratio - the probability of setting swich to R
+%   returns railway graph with wieghts corresponding to deirections as
+%   follows: 1 - 0; 2 - L; 3 - R.
 
 if nargin < 1
     size = 6;
@@ -17,21 +19,59 @@ end
 
 N = 2 * size;
 
-W = zeros(N);
+%% generate 3-regular graph 
+
+A = zeros(N);
 
 j = 1;
 for i = 1:N
     
-    for k = 1:(3 - sum(W(i, :)))
+    for k = 1:(3 - sum(A(i, :)))
         
         j = j + 1;
         if j > N
             j = i + 1;
         end
         
-        W(i, j) = 1;
-        W(j, i) = 1;
+        A(i, j) = 1;
+        A(j, i) = 1;
         
     end
     
 end
+
+%% set L, R and 0 directions
+
+for i = 1:N
+    
+    for j = 1:N
+        
+        if A(i, j) ~= 0
+            A(i, j) = sum(A(i, :)) - A(i, j);
+        end
+        
+        if sum(A(i, :)) == 6
+            break;
+        end
+        
+    end
+    
+end
+
+%% set switches
+
+for i = 1:N
+    
+    if rand(1) < ratio
+        s = 2;
+    else
+        s = 3;
+    end
+    
+    A(i, A(i, :) == s) = 0;
+    
+end
+
+railway = digraph(A);
+plot(railway, 'EdgeLabel', railway.Edges.Weight);
+
